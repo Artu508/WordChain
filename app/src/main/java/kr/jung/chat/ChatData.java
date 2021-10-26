236fe818b6fb15b23f834e59a11406a4244da8c0
jung4337 <monkey4337@gmail.com>
2021-10-26 13:50:49 +0900

    Initial commit

---
+++ app/src/main/java/kr/jung/chat/ChatData.java
@@ -0,0 +1,27 @@
+package kr.jung.chat;
+
+public class ChatData {
+
+    /*
+    * DTO Data Transform Objects
+    */
+
+    private String msg;
+    private String nickname;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
---
+++ app/src/main/java/kr/jung/chat/MainActivity.java
@@ -0,0 +1,109 @@
+package kr.jung.chat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    public  RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<ChatData> chatList;
+    private String nick = "nick2";
+
+    private EditText ed_Chat;
+    private Button btn_Send;
+    private DatabaseReference db_Ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn_Send = findViewById(R.id.btn_Send);
+        ed_Chat = findViewById(R.id.ed_Chat);
+
+        btn_Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = ed_Chat.getText().toString(); //msg
+
+                if(msg != null) {
+                    ed_Chat.setText(null);
+                    ChatData chat = new ChatData();
+                    chat.setNickname(nick);
+                    chat.setMsg(msg);
+                    db_Ref.push().setValue(chat);
+                }
+
+            }
+        });
+
+
+
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        chatList = new ArrayList<>();
+        mAdapter = new ChatAdapter(chatList, MainActivity.this, nick);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        db_Ref = database.getReference();
+
+
+        //caution!!!
+
+        db_Ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("CHATCHAT", dataSnapshot.getValue().toString());
+                ChatData chat = dataSnapshot.getValue(ChatData.class);
+                ((ChatAdapter) mAdapter).addChat(chat);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
