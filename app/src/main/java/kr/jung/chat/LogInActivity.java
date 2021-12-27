8aba7a1f023676b685836713be490703bfd59285
jung4337 <monkey4337@gmail.com>
2021-12-27 17:13:29 +0900

    login, signup

--- app/src/main/java/kr/jung/chat/LogInActivity.java
+++ app/src/main/java/kr/jung/chat/LogInActivity.java
@@ -1,9 +1,12 @@
 package kr.jung.chat;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +29,12 @@ public class LogInActivity extends AppCompatActivity {
     private EditText editTextPassword;
     private Button buttonLogIn;
     private TextView buttonSignUp;
+
+    private CheckBox cbAutoLogin;
+    private SharedPreferences loginData;
+    private boolean saveLoginData;
+    private String id;
+    private String pwd;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,11 @@ public class LogInActivity extends AppCompatActivity {
 
         editTextEmail = (EditText) findViewById(R.id.editText_email);
         editTextPassword = (EditText) findViewById(R.id.editText_passWord);
- 
+
+        cbAutoLogin = (CheckBox) findViewById(R.id.cb_autologin);
+
+        saveLoginData();
+
         buttonSignUp = (TextView) findViewById(R.id.btn_signup);
         buttonSignUp.setOnClickListener(v -> {
             // SignUpActivity 연결
@@ -47,7 +60,7 @@ public class LogInActivity extends AppCompatActivity {
         buttonLogIn = (Button) findViewById(R.id.btn_login);
         buttonLogIn.setOnClickListener(v -> {
             if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
-                loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+                loginUser(editTextEmail.getText().toString(), editTextPassword.getText().toString(), cbAutoLogin.isChecked());
             } else {
                 Toast.makeText(LogInActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
             }
@@ -64,20 +77,52 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
-    public void loginUser(String email, String password) {
+    public void loginUser(String email, String password, boolean checked) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         // 로그인 성공
                         Toast.makeText(LogInActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
                         firebaseAuth.addAuthStateListener(firebaseAuthListener);
+                        if (checked){
+                            setLoginData();
+                        }
                     } else {
                         // 로그인 실패
                         Toast.makeText(LogInActivity.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
- 
+
+    private void saveLoginData(){
+        loginData = getSharedPreferences("loginData", MODE_PRIVATE);
+        loadData();
+
+        if (saveLoginData) {
+            editTextEmail.setText(id);
+            editTextPassword.setText(pwd);
+            cbAutoLogin.setChecked(saveLoginData);
+        }
+
+    }
+
+    private void setLoginData(){
+        SharedPreferences.Editor editor = loginData.edit();
+
+        editor.putBoolean("SAVE_LOGIN_DATA", cbAutoLogin.isChecked());
+        editor.putString("ID", editTextEmail.getText().toString().trim());
+        editor.putString("PWD", editTextPassword.getText().toString().trim());
+
+        editor.apply();
+
+    }
+
+    private void loadData() {
+        saveLoginData = loginData.getBoolean("SAVE_LOGIN_DATA", false);
+        id = loginData.getString("ID", "");
+        pwd = loginData.getString("PWD", "");
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
